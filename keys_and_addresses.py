c096f441c1a573759988716f4c88317e84375467
forkonlyone <78365966+forkonlyone@users.noreply.github.com>
2025-01-14 12:33:27 +0700

    Create keys_and_addresses.py

---
+++ keys_and_addresses.py
@@ -0,0 +1,13 @@
+# keys_and_addresses.py
+# replace you key and address 
+private_keys = [
+    'PRIVATEKEy'  # Example: '9da...365'
+]
+
+my_addresses = [
+    '0x9e4cd4eEcbf62d4178a72E0383fc540C0E913257'  # Example: '0x087....E85'
+]
+
+labels = [
+    'my_address'
+]
